d8f3e18f33552c4e10f758686aafe7ddf8b82a0b
Kevin Devine <kevin@picnet.net>
2025-02-18 18:57:02 -0500

    add home link to pageheader

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -3,7 +3,9 @@ function PageHeader() {
     <div class="flex flex-col items-center gap-y-1 mb-2">
       <div class="flex items-center gap-x-2">
         <div class="i-horizon-logo"></div>
-        <div>bsky - horizon</div>
+        <div>
+          <a href="/">bsky - horizon</a>
+        </div>
       </div>
     </div>
   )
